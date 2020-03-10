50b2a5742e75852f6adc44cb91f338298e62be4f
d3nnywild <59361131+d3nnywild@users.noreply.github.com>
2020-03-10 16:10:28 +0100

    Add files via upload

---
+++ hangman/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "../../vsgl2.h"
+#include <ctime>
+#include <stdlib.h>
+
+using namespace std;
+using namespace vsgl2;
+using namespace vsgl2::general;
+using namespace vsgl2::video;
+using namespace vsgl2::utils;
+
+int main(int argc, char* argv[])
+{
+    init();
+    srand(time(NULL));
+    set_window(1240,720,"Hangman");
+    while(!done())
+    {
+        set_background_color(Color(25,25,25,255));
+
+        //number = to_string(points);
+        //draw_text("Roboto-Thin.ttf",40,"Score",WIDTH/2-40,-5,Color(255,255,255,255));
+
+        update();
+
+        delay(5);
+    }
+    close();
+    return 0;
+}
